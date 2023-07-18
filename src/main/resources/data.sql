95654148dd56795559a6c0846cbb164eea48fd4e
LeooZeballos <zeballosleonel3@gmail.com>
2023-07-18 16:02:00 -0300

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,65 +1,50 @@
 -- Recursos
-INSERT INTO RECURSO (NOMBRE, DESCRIPCION) VALUES ('Proyector', 'Un proyector HD');
-INSERT INTO RECURSO (NOMBRE, DESCRIPCION) VALUES ('Pizarrón', 'Color negro');
-INSERT INTO RECURSO (NOMBRE, DESCRIPCION) VALUES ('Silla', 'Con cuatro pata');
-INSERT INTO RECURSO (NOMBRE, DESCRIPCION) VALUES ('Sillón', 'Color marrón');
+INSERT INTO RECURSO (NOMBRE, DESCRIPCION)
+VALUES ('Proyector', 'Un proyector HD'),
+       ('Pizarrón', 'Color negro'),
+       ('Silla', 'Con cuatro patas'),
+       ('Sillón', 'Color marrón');
 
 -- Espacio Fisico
-INSERT INTO ESPACIO_FISICO (NOMBRE, CAPACIDAD, DESCRIPCION, HABILITADO) VALUES ('Espacio 1', 20, '...', true);
-INSERT INTO ESPACIO_FISICO (NOMBRE, CAPACIDAD, DESCRIPCION, HABILITADO) VALUES ('Espacio 2', 20, '...', true);
-INSERT INTO ESPACIO_FISICO (NOMBRE, CAPACIDAD, DESCRIPCION, HABILITADO) VALUES ('Espacio 3', 20, '...', true);
-INSERT INTO ESPACIO_FISICO (NOMBRE, CAPACIDAD, DESCRIPCION, HABILITADO) VALUES ('Espacio 4', 20, '...', true);
-INSERT INTO ESPACIO_FISICO (NOMBRE, CAPACIDAD, DESCRIPCION, HABILITADO) VALUES ('Espacio 5', 20, '...', true);
+INSERT INTO ESPACIO_FISICO (NOMBRE, CAPACIDAD, DESCRIPCION, HABILITADO)
+VALUES ('Espacio 1', 20, '...', true),
+       ('Espacio 2', 20, '...', true),
+       ('Espacio 3', 20, '...', true),
+       ('Espacio 4', 20, '...', true),
+       ('Espacio 5', 20, '...', true);
 
 -- Recursos del Espacio Físico
 INSERT INTO ESPACIO_FISICO_RECURSOS (ESPACIO_FISICO_ID, RECURSOS_ID)
-VALUES (1, 1);
-INSERT INTO ESPACIO_FISICO_RECURSOS (ESPACIO_FISICO_ID, RECURSOS_ID)
-VALUES (1, 2);
-INSERT INTO ESPACIO_FISICO_RECURSOS (ESPACIO_FISICO_ID, RECURSOS_ID)
-VALUES (1, 3);
-INSERT INTO ESPACIO_FISICO_RECURSOS (ESPACIO_FISICO_ID, RECURSOS_ID)
-VALUES (2, 2);
-INSERT INTO ESPACIO_FISICO_RECURSOS (ESPACIO_FISICO_ID, RECURSOS_ID)
-VALUES (3, 3);
-INSERT INTO ESPACIO_FISICO_RECURSOS (ESPACIO_FISICO_ID, RECURSOS_ID)
-VALUES (4, 4);
-INSERT INTO ESPACIO_FISICO_RECURSOS (ESPACIO_FISICO_ID, RECURSOS_ID)
-VALUES (5, 4);
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (2, 2),
+       (3, 3),
+       (4, 4),
+       (5, 4);
 
 -- Estados
 INSERT INTO ESTADO (NOMBRE, DESCRIPCION, COLOR)
-VALUES ('Creado', 'La reserva fue creada de manera exitosa.', '#FFA500');
-INSERT INTO ESTADO (NOMBRE, DESCRIPCION, COLOR)
-VALUES ('Reservado', 'La reserva quedo grabada correctamente.', '#008000');
-INSERT INTO ESTADO (NOMBRE, DESCRIPCION, COLOR)
-VALUES ('Pendiente', 'La reserva está pendiente.', '#FFD700');
-INSERT INTO ESTADO (NOMBRE, DESCRIPCION, COLOR)
-VALUES ('Cancelada', 'La reserva fue cancelada.', '#FF0000');
-INSERT INTO ESTADO (NOMBRE, DESCRIPCION, COLOR)
-VALUES ('Completada', 'La reserva fue completada.', '#00FF00');
-INSERT INTO ESTADO (NOMBRE, DESCRIPCION, COLOR)
-VALUES ('Bloqueada', 'La reserva fue bloqueada.', '#800000');
+VALUES ('Creado', 'La reserva fue creada de manera exitosa.', '#FFA500'),
+       ('Reservado', 'La reserva quedó grabada correctamente.', '#008000'),
+       ('Pendiente', 'La reserva está pendiente.', '#FFD700'),
+       ('Cancelada', 'La reserva fue cancelada.', '#FF0000'),
+       ('Completada', 'La reserva fue completada.', '#00FF00'),
+       ('Bloqueada', 'La reserva fue bloqueada.', '#800000');
 
 -- Rol
 INSERT INTO ROL (NOMBRE, DESCRIPCION)
-VALUES ('Administrador', 'Administrador del sistema.');
-INSERT INTO ROL (NOMBRE, DESCRIPCION)
-VALUES ('Usuario', 'Usuario del sistema.');
-INSERT INTO ROL (NOMBRE, DESCRIPCION)
-VALUES ('Cliente', '');
-INSERT INTO ROL (NOMBRE, DESCRIPCION)
-VALUES ('Docente', '');
+VALUES ('Administrador', 'Administrador del sistema.'),
+       ('Usuario', 'Usuario del sistema.'),
+       ('Cliente', ''),
+       ('Docente', '');
 
 -- Clientes
 INSERT INTO CLIENTE (NOMBRE, APELLIDO, NRO_TELEFONO, EMAIL, DNI, ROL_ID)
-VALUES ('Leonel', 'Zeballos', '12345678', 'zeballosleonel3@gmail.com', '12345678', 1);
-INSERT INTO CLIENTE (NOMBRE, APELLIDO, NRO_TELEFONO, EMAIL, DNI, ROL_ID)
-VALUES ('Juan', 'Perez', '12345678', 'juanperez@gmail.com', '12345678', 3);
-INSERT INTO CLIENTE (NOMBRE, APELLIDO, NRO_TELEFONO, EMAIL, DNI, ROL_ID)
-VALUES ('Pedro', 'Gonzalez', '12345678', 'pedrogonzalez@gmail.com', '12345678', 2);
-INSERT INTO CLIENTE (NOMBRE, APELLIDO, NRO_TELEFONO, EMAIL, DNI, ROL_ID)
-VALUES ('Maria', 'Gomez', '12345678', 'mariagomez@gmail.com', '12345678', 4);
+VALUES ('Leonel', 'Zeballos', '12345678', 'zeballosleonel3@gmail.com', '12345678', 1),
+       ('Juan', 'Perez', '12345678', 'juanperez@gmail.com', '12345678', 3),
+       ('Pedro', 'Gonzalez', '12345678', 'pedrogonzalez@gmail.com', '12345678', 2),
+       ('Maria', 'Gomez', '12345678', 'mariagomez@gmail.com', '12345678', 4);
 
 -- Reservas
 INSERT INTO RESERVA (FECHA_HORA_INICIO, FECHA_HORA_FIN, COMENTARIO, FECHA_HORA_CREACION, CLIENTE_ID, MOTIVO_RESERVA,
